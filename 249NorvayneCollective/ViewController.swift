6d8f1b2f998e3897e460881c4ed484242bf8c5e9
Roman <romazaniosdev@gmail.com>
2026-07-15 21:58:21 +0300

    Initial Commit

---
+++ 249NorvayneCollective/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  249NorvayneCollective
+//
+//  Created by Roman on 7/15/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
